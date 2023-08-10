b37c8913fa8f315253c178413a2e1dfbd15fb1ab
wonseok <kws@bankq.com>
2023-08-10 14:14:17 +0900

    로그인 2

---
+++ src/main/java/com/study/board/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.study.board.domain;
+
+public enum  UserRole {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/study/board/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.study.board.repository;
+
+import com.study.board.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickname);
+    Optional<User> findByLoginId(String loginId);
+}
